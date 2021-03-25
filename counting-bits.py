98b5822eea1f5f40d9c6420e52e3b9dbc346d55c
Pavitra Walia <waliapavitra@gmail.com>
2021-03-25 21:07:12 +0530

    Create counting-bits.py

---
+++ counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        dp = [0] * (num + 1)
+        for i in range(1, num+1):
+            if i % 2 == 1:
+                dp[i] = dp[i//2] + 1
+            else:
+                dp[i] = dp[i//2]
+        return dp
